c46868365a83654e38cc56d7e23643d283a9c0df
Esteban <estebanzeb@gmail.com>
2024-11-06 18:53:37 -0500

    Ejemplo

---
+++ hogares/hogares/lib_aplicaciones/Interfaces/ICasasAplicacion.cs
@@ -0,0 +1,14 @@
+﻿using lib_entidades.Modelos;
+
+namespace lib_aplicaciones.Interfaces
+{
+    public interface ICasasAplicacion
+    {
+        void Configurar(string string_conexion);
+        List<Casas> Buscar(Casas entidad, string tipo);
+        List<Casas> Listar();
+        Casas Guardar(Casas entidad);
+        Casas Modificar(Casas entidad);
+        Casas Borrar(Casas entidad);
+    }
+}
